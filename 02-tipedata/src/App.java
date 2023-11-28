d2715942bf8df4a22890a98eca37616b6724d30d
ILHAMPARHADI <146052126+ILHAMPARHADI@users.noreply.github.com>
2023-11-28 13:14:42 +0800

    tipe tgl/ 28 november 2023

---
+++ 02-tipedata/src/App.java
@@ -0,0 +1,72 @@
+public class App  {
+    public static void main(String[] args) throws Exception {
+        
+// tipe data di java
+// integer, byte, short, long, double, float, char, boolean
+         
+// integer (satuan)
+int i = 10  ;
+system.out.println("=====INTEGER=====");
+system.out.println("nilai integer i = " + i );
+system.out.println("nilai MAX : " + integer .MAX_VALUE);
+system.out.ptintln("nilai MIN : " + Integer .MIN_VALUE_VALUE);
+system.out.println("besar integer : " + integer .SIZE + "bit" );
+
+// byte
+byte b=10;
+system.out.println("=====byte=====");
+system.out.println("nilai byte b = " + b );
+system.out.println("nilai MAX : " + byte .MAX_VALUE);
+system.out.ptintln("nilai MIN : " + byte .MIN_VALUE_VALUE);
+system.out.println("besar byte : " + byte .SIZE + "bit" );
+
+//short
+short s=10;
+system.out.println("=====short=====");
+system.out.println("nilai short s = " + s );
+system.out.println("nilai MAX : " + short .MAX_VALUE);
+system.out.ptintln("nilai MIN : " + short .MIN_VALUE_VALUE);
+system.out.println("besar short : " + short .SIZE + "bit" );
+
+//long
+long l=10;
+system.out.println("=====long=====");
+system.out.println("nilai long l = " + l );
+system.out.println("nilai MAX : " + long .MAX_VALUE);
+system.out.ptintln("nilai MIN : " + long .MIN_VALUE_VALUE);
+system.out.println("besar long : " + long .SIZE + "bit" );
+
+//double
+double d=10;
+system.out.println("=====double=====");
+system.out.println("nilai double d  = " + d );
+system.out.println("nilai MAX : " + double .MAX_VALUE);
+system.out.ptintln("nilai MIN : " + double .MIN_VALUE_VALUE);
+system.out.println("besar double : " + double .SIZE + "bit" );
+
+//float
+float f=10;
+system.out.println("=====float=====");
+system.out.println("nilai float f = " + f );
+system.out.println("nilai MAX : " + float .MAX_VALUE);
+system.out.ptintln("nilai MIN : " + float .MIN_VALUE_VALUE);
+system.out.println("besar float: " + float .SIZE + "bit" );
+
+//char
+char c=10;
+system.out.println("=====char=====");
+system.out.println("nilai char c = " + c );
+system.out.println("nilai MAX : " + char .MAX_VALUE);
+system.out.ptintln("nilai MIN : " + char .MIN_VALUE_VALUE);
+system.out.println("besar char : " + char .SIZE + "bit" );
+
+//boolean
+boolean b=10;
+system.out.println("=====boolean=====");
+system.out.println("nilai boolean b = " + b );
+system.out.println("nilai MAX : " + boolean .MAX_VALUE);
+system.out.ptintln("nilai MIN : " + boolean .MIN_VALUE_VALUE);
+system.out.println("besar boolean : " + boolean .SIZE + "bit" );
+
+
+
